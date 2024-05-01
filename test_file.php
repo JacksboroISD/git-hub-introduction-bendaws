1eeece433543cd1b31760c19df03e6b6df132b17
Ben Daws <bendawsdev@gmail.com>
2024-05-01 13:04:19 -0500

    Create test_file.php

---
+++ test_file.php
@@ -0,0 +1,13 @@
+<!-- This is only test PHP file! -->
+
+<html>
+  <head>
+    <title>Test page (jisd)</title>
+  </head>
+
+  <body>
+    <?php
+      echo "Hello world!";
+    ?>
+  </body>
+</html>
